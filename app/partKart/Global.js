baf22f6f791f740fe9e44a74de5287b75ce1cf6a
John Talbot <ztalbot2000@gmail.com>
2019-11-10 14:30:07 -0500

    Prove getAngle works

--- app/partKart/Global.js
+++ app/partKart/Global.js
@@ -1,228 +1,281 @@
 ﻿//package com.partkart{
-//;(function () { // wrapper in case we're in module_context mode
 
-   // import flash.geom.Point;
-   //import { Point } from 'pixi';
-   //import { Point } from 'pixi.js';
-   import { Point } from '@pixi/math';
+import { Point } from '@pixi/math';
 
-   //public class Global
+Point.prototype.normalize = function(scale){
+  var norm = Math.sqrt(this.x * this.x + this.y * this.y);
+  if (norm != 0) {
+     this.x = scale * this.x / norm;
+     this.y = scale * this.y / norm;
+  }
+}
+
+//public class Global
 class GlobalClass
+{
+   constructor()
    {
-      constructor(name) {
-         this.name = name;
-         this.zarf = name;
 
-         //public static var zoom:Number = 80; // default resolution of 80 pixels per inch
-         this.zoom = 80; // default resolution of 80 pixels per inch
+      //public static var zoom:Number = 80; // default resolution of 80 pixels per inch
+      this.zoom = 80; // default resolution of 80 pixels per inch
       //public static var tool:int = 0; // default tool is pointer
-         this.tool = 0; // default tool is pointer
+      this.tool = 0; // default tool is pointer
 
       //public static var snap:Boolean = false;
-         this.snap = false;
+      this.snap = false;
 
       //public static var xorigin:Number = 0;
-         this.xorigin = 0;
+      this.xorigin = 0;
       //public static var yorigin:Number = 600;
-         this.yorigin = 600;
+      this.yorigin = 600;
 
       //public static var docwidth:int = 950;
-         this.docwidth = 950;
+      this.docwidth = 950;
       //public static var docheight:int = 600;
-         this.docheight = 600;
+      this.docheight = 600;
 
       //public static var dragging:Boolean = false; // captures all dragging events, including dots, paths and main window dragging
-         this.dragging = false; // captures all dragging events, including dots, paths and main window dragging
+      this.dragging = false; // captures all dragging events, including dots, paths and main window dragging
 
       //public static var unit:String = "in"; // "in" = inches "cm" = centimeters. Stop using magic values!!
-         this.unit = "in"; // "in" = inches "cm" = centimeters. Stop using magic values!!
+      this.unit = "in"; // "in" = inches "cm" = centimeters. Stop using magic values!!
 
       //public static var space:Boolean = false; // true if space is currently down
-         this.space = false; // true if space is currently down
+      this.space = false; // true if space is currently down
 
       //public static var importres:Number = 72;
-         this.importres = 72;
+      this.importres = 72;
       //public static var localsnap:Boolean = true;
-         this.localsnap = true;
-      
+      this.localsnap = true;
+   
       //public static var tolerance:Number = 0.001; // global tolerance for cam operations
-         this.tolerance = 0.001; // global tolerance for cam operations
+      this.tolerance = 0.001; // global tolerance for cam operations
       //public static var precision:int = 4;
-         this.precision = 4;
+      this.precision = 4;
       //public static var bitmaptolerance:Number = 0.007;
-         this.bitmaptolerance = 0.007;
+      this.bitmaptolerance = 0.007;
       //public static var bitmapsize:Number = 4000;
-         this.bitmapsize = 4000;
+      this.bitmapsize = 4000;
       //public static var nestbitmapsize:Number = 1000;
-         this.nestbitmapsize = 1000;
+      this.nestbitmapsize = 1000;
 
       //public static var separatetoolpaths:Boolean = false;
-         this.separatetoolpaths = false;
+      this.separatetoolpaths = false;
 
       //public static var viewcuts:Boolean = false;
-         this.viewcuts = false;
+      this.viewcuts = false;
 
       // undo stack
       //public static var undo:Array = new Array();
-         this.undo = new Array();
+      this.undo = new Array();
       //private static var undoindex:int = 0;
-         this.undoindex = 0;
+      this.undoindex = 0;
 
+   }
+
+   //public static function undoPush(u:Undo):void
+   undoPush(u)
+   {
+      undo = undo.slice(0,undoindex+1);
+      undo.push(u);
+      if(undo.length > 10){
+         undo.shift();
       }
+      undoindex = undo.length-1;
+   }
 
-      build() {
-        return new GlobalClass(this);
-      }
-      //public static function undoPush(u:Undo):void
-      static undoPush(u)
-      {
-         undo = undo.slice(0,undoindex+1);
-         undo.push(u);
-         if(undo.length > 10){
-            undo.shift();
-         }
-         undoindex = undo.length-1;
-      }
-
-      //public static function undoAction():void
-      static undoAction()
-      {
-         if(undoindex >= 0 && undoindex < undo.length){
-            undo[undoindex].undoAction();
-            undoindex--;
-         }
-      }
-
-      //public static function redoAction():void
-      static redoAction()
-      {
-         if(undoindex >= -1 && undoindex < undo.length-1){
-            undoindex++;
-            undo[undoindex].redoAction();
-         }
-      }
-
-      //public static function withinTolerance(p1:Point, p2:Point, factor:Number = 1):Boolean
-      static withinTolerance(p1, p2, factor = 1)
-      {
-         //var diff:Point = new Point(Math.pow(p1.x-p2.x,2),Math.pow(p1.y-p2.y,2));
-         var diff = new Point(Math.pow(p1.x-p2.x,2),Math.pow(p1.y-p2.y,2));
-         if(diff.x + diff.y < Math.pow(Global.tolerance*factor,2)){
-            return true;
-         }
-         return false;
-      }
-
-      /*
-      This gives very wonky results with metric, so simplify the 'precision' param 
-      to indicate a 'decimal place precision', much simpler for GRBL owners
-      See this SO question:
-      http://stackoverflow.com/questions/632802/how-to-deal-with-number-precision-in-actionscript
-      
-      public static function toFixed(number:Number, factor:Number):Number{
-         return (Math.round(number * factor)/factor);
-      }*/
-      //public static function toFixed(number:Number, precision:int)
-      static toFixed(number, precision)
-      {
-       precision = Math.pow(10, precision);
-       return Math.round(number * precision)/precision;
-      }
-      
-      // returns the angle between two vectors
-      //public static function getAngle(v1:Point, v2:Point):Number
-      static getAngle(v1, v2)
-      {
-         v1.normalize(1);
-         v2.normalize(1);
-
-         //var crossproduct:Number = v1.x*v2.y - v1.y*v2.x;
-         var crossproduct = v1.x*v2.y - v1.y*v2.x;
-         //var dotproduct:Number = v1.x*v2.x + v1.y*v2.y;
-         var dotproduct = v1.x*v2.x + v1.y*v2.y;
-
-         //using both cross and dot product to calculate angle avoids numerical error caused by acos/asin at roots
-         //var angle:Number = Math.atan2(crossproduct,dotproduct);
-         var angle = Math.atan2(crossproduct,dotproduct);
-
-         return angle;
-      }
-
-      //---------------------------------------------------------------
-      //Checks for intersection of Segment if as_seg is true.
-      //Checks for intersection of Line if as_seg is false.
-      //Return intersection of Segment AB and Segment EF as a Point
-      //Return null if there is no intersection
-      //---------------------------------------------------------------
-      //public static function lineIntersect(A:Point,B:Point,E:Point,F:Point,as_seg:Boolean=false):Point 
-      static lineIntersect(A,B,E,F,as_seg=false)
-      {
-         //var ip:Point;
-         var ip;
-         //var a1:Number;
-         var a1;
-         //var a2:Number;
-         var a2;
-         //var b1:Number;
-         var b1;
-         //var b2:Number;
-         var b2;
-         //var c1:Number;
-         var c1;
-         //var c2:Number;
-         var c2;
-
-         a1= B.y-A.y;
-         b1= A.x-B.x;
-         c1= B.x*A.y - A.x*B.y;
-         a2= F.y-E.y;
-         b2= E.x-F.x;
-         c2= F.x*E.y - E.x*F.y;
-
-         //var denom:Number=a1*b2 - a2*b1;
-         var denom = a1*b2 - a2*b1;
-         if (denom == 0) {
-            return null;
-         }
-         ip=new Point();
-         ip.x=(b1*c2 - b2*c1)/denom;
-         ip.y=(a2*c1 - a1*c2)/denom;
-
-          //      Deal with rounding errors.
-
-         if ( A.x == B.x )
-               ip.x = A.x;
-         else if ( E.x == F.x )
-               ip.x = E.x;
-         if ( A.y == B.y )
-               ip.y = A.y;
-         else if ( E.y == F.y )
-               ip.y = E.y;
-
-
-         //---------------------------------------------------
-         //Do checks to see if intersection to endpoints
-         //distance is longer than actual Segments.
-         //Return null if it is with any.
-         //---------------------------------------------------
-         if(as_seg){
-            if (Math.abs(A.x-B.x) > 0.000000000001 && (( A.x < B.x ) ? ip.x < A.x || ip.x > B.x : ip.x > A.x || ip.x < B.x ))
-                      return null;
-            if (Math.abs(A.y-B.y) > 0.000000000001 && (( A.y < B.y ) ? ip.y < A.y || ip.y > B.y : ip.y > A.y || ip.y < B.y ))
-               return null;
-
-            if (Math.abs(E.x-F.x) > 0.000000000001 && (( E.x < F.x ) ? ip.x < E.x || ip.x > F.x : ip.x > E.x || ip.x < F.x ))
-               return null;
-            if (Math.abs(E.y-F.y) > 0.000000000001 && (( E.y < F.y ) ? ip.y < E.y || ip.y > F.y : ip.y > E.y || ip.y < F.y ))
-               return null;
-         }
-
-         if(isNaN(ip.x) || isNaN(ip.y)){
-            return null;
-         }
-
-         return ip;
+   //public static function undoAction():void
+   undoAction()
+   {
+      if(undoindex >= 0 && undoindex < undo.length){
+         undo[undoindex].undoAction();
+         undoindex--;
       }
    }
-//}
-export default  new GlobalClass("blast");
+
+   //public static function redoAction():void
+   redoAction()
+   {
+      if(undoindex >= -1 && undoindex < undo.length-1){
+         undoindex++;
+         undo[undoindex].redoAction();
+      }
+   }
+
+   //public static function withinTolerance(p1:Point, p2:Point, factor:Number = 1):Boolean
+   withinTolerance(p1, p2, factor = 1)
+   {
+      //var diff:Point = new Point(Math.pow(p1.x-p2.x,2),Math.pow(p1.y-p2.y,2));
+      var diff = new Point(Math.pow(p1.x-p2.x,2),Math.pow(p1.y-p2.y,2));
+      if(diff.x + diff.y < Math.pow(Global.tolerance*factor,2)){
+         return true;
+      }
+      return false;
+   }
+
+   /*
+   This gives very wonky results with metric, so simplify the 'precision' param 
+   to indicate a 'decimal place precision', much simpler for GRBL owners
+   See this SO question:
+   http://stackoverflow.com/questions/632802/how-to-deal-with-number-precision-in-actionscript
+   
+   public static function toFixed(number:Number, factor:Number):Number{
+      return (Math.round(number * factor)/factor);
+   }*/
+   //public static function toFixed(number:Number, precision:int)
+   toFixed(number, precision)
+   {
+      precision = Math.pow(10, precision);
+      return Math.round(number * precision)/precision;
+   }
+   
+   //---------------------------------------------------------------
+   //
+   // getAngle
+   //    returns the angle between two vectors (in radians)
+   //
+   // Note: This is not the angle *OF* the two vectors.  The angle
+   //       between two vectors is that angle of both relative to
+   //       the point (0,0).
+   //                       |     .(5,13)
+   //                       |    .
+   //                       |   .
+   //                       |  . \ θ       . (6,3)
+   //                       | .   \   .
+   //                  (0,0)|.  .         
+   //           -----------------------------------
+   //                       |
+   //                       |
+   //
+   // Note: The two vectors are minimized. This Scales the line
+   //       segment between (0,0) and the current point.
+   //
+   // Note: To convert to degrees you would mutiply by:
+   //        180 / (4 * arctangent (1))
+   //
+   // Example:
+   //   Find the angle θ between u =〈6,3) and v =〈5,13〉
+   //   Step 1: Find the Dot product u·v = u1v1+u2v2
+   //           (6·5)+(3·13)
+   //             30 +  39 = 69
+   // 
+   //   Step 2: Find the magnitudes of each vector
+   //           ||u|| = √( u1·u1 + u2·u2 )
+   //           ||u|| = √( 6·6 + 5·5 )
+   //           ||u|| = √45 = 3√5
+   //
+   //           ||v|| = √( v1·v1 + v2·v2 )
+   //           ||v|| = √( 5·5 + 13·13)
+   //           ||v|| = √194
+   //
+   //   Step 3: Substitute and solve for θ.
+   //           cos θ = u·v / ||u|| · ||v||
+   //           cos θ = 69  /  3√5  ·  √194  = 23/√970
+   //
+   //           θ = cos−1 23/√970
+   //           θ ≈ .73948892455246225752
+   //
+   //   Step 4: Convert from rads to degrees
+   //           θ = .73948892455246225752 * 180 / (4 * arctangent (1))
+   //           θ ≈ 42°
+   // 
+   //     Using 'bc -l' this would c(23/sqrt(970)) * 180/(4*a(1))
+
+   //public static function getAngle(v1:Point, v2:Point):Number
+   getAngle(v1, v2)
+   {
+      // Scales the line segment between (0,0) and the current point
+      // to a set length.
+      v1.normalize(1);
+      v2.normalize(1);
+
+      //var crossproduct:Number = v1.x*v2.y - v1.y*v2.x;
+      var crossproduct = v1.x*v2.y - v1.y*v2.x;
+      //var dotproduct:Number = v1.x*v2.x + v1.y*v2.y;
+      var dotproduct = v1.x*v2.x + v1.y*v2.y;
+
+      // Using both cross and dot product to calculate angle avoids
+      // numerical error caused by acos/asin at roots
+      //var angle:Number = Math.atan2(crossproduct,dotproduct);
+      var angle = Math.atan2(crossproduct,dotproduct);
+
+      return angle;
+   }
+
+   //---------------------------------------------------------------
+   //Checks for intersection of Segment if as_seg is true.
+   //Checks for intersection of Line if as_seg is false.
+   //Return intersection of Segment AB and Segment EF as a Point
+   //Return null if there is no intersection
+   //---------------------------------------------------------------
+   //public static function lineIntersect(A:Point,B:Point,E:Point,F:Point,as_seg:Boolean=false):Point 
+   lineIntersect(A,B,E,F,as_seg=false)
+   {
+      //var ip:Point;
+      var ip;
+      //var a1:Number;
+      var a1;
+      //var a2:Number;
+      var a2;
+      //var b1:Number;
+      var b1;
+      //var b2:Number;
+      var b2;
+      //var c1:Number;
+      var c1;
+      //var c2:Number;
+      var c2;
+
+      a1= B.y-A.y;
+      b1= A.x-B.x;
+      c1= B.x*A.y - A.x*B.y;
+      a2= F.y-E.y;
+      b2= E.x-F.x;
+      c2= F.x*E.y - E.x*F.y;
+
+      //var denom:Number=a1*b2 - a2*b1;
+      var denom = a1*b2 - a2*b1;
+      if (denom == 0) {
+         return null;
+      }
+      ip=new Point();
+      ip.x=(b1*c2 - b2*c1)/denom;
+      ip.y=(a2*c1 - a1*c2)/denom;
+
+      //      Deal with rounding errors.
+
+      if ( A.x == B.x )
+            ip.x = A.x;
+      else if ( E.x == F.x )
+            ip.x = E.x;
+      if ( A.y == B.y )
+            ip.y = A.y;
+      else if ( E.y == F.y )
+            ip.y = E.y;
+
+
+      //---------------------------------------------------
+      //Do checks to see if intersection to endpoints
+      //distance is longer than actual Segments.
+      //Return null if it is with any.
+      //---------------------------------------------------
+      if(as_seg){
+         if (Math.abs(A.x-B.x) > 0.000000000001 && (( A.x < B.x ) ? ip.x < A.x || ip.x > B.x : ip.x > A.x || ip.x < B.x ))
+                   return null;
+         if (Math.abs(A.y-B.y) > 0.000000000001 && (( A.y < B.y ) ? ip.y < A.y || ip.y > B.y : ip.y > A.y || ip.y < B.y ))
+            return null;
+
+         if (Math.abs(E.x-F.x) > 0.000000000001 && (( E.x < F.x ) ? ip.x < E.x || ip.x > F.x : ip.x > E.x || ip.x < F.x ))
+            return null;
+         if (Math.abs(E.y-F.y) > 0.000000000001 && (( E.y < F.y ) ? ip.y < E.y || ip.y > F.y : ip.y > E.y || ip.y < F.y ))
+            return null;
+      }
+
+      if(isNaN(ip.x) || isNaN(ip.y)){
+         return null;
+      }
+
+      return ip;
+   }
+}
+export default  new GlobalClass();
--- test/partKart/Globals.js
+++ test/partKart/Globals.js
@@ -1,70 +1,9 @@
 //'use strict';
 
-//const exec = require('child_process').exec;
-//const ps = require('child_process').exec;
-
-//w const jsdom = require("jsdom");
-//w const { JSDOM } = jsdom;
-//w var window = (new JSDOM(``, { pretendToBeVisual: true })).window;
-//w window.requestAnimationFrame(timestamp => {
-//w   console.log(timestamp > 0);
-//w });
-
-
-//w global.document = window.document;
-//w global.navigator = {
-//w   userAgent: 'node.js',
-//w };
-
-
-//w const PIXI = require('pixi.js');
-
 var assert = require('chai').assert;
 
-// ***************** TEST LOADING **********************
-
-
-function dynamicallyLoadScript( url )
-{
-   return new Promise (function(resolve, reject)
-   {
-      var script = document.createElement("script" );
-      script.src = url;
-      script.onload = resolve;
-      script.onerror = () => reject(new Error(`Error when loading $\{url\}!`));
-      document.body.appendChild(script);
-   });
-}
-// Cannot load resource /users/zarf/XCode_dev_t/Global.js
-//dynamicallyLoadScript("../app/js/com/partKart/Global.js");
-// Cannot use import outside a module
-//dynamicallyLoadScript("/Users/zarf/XcodeDevelopment/makercam5/app/js/com/partKart/Global.js");
-
-// Worked
-// import Global  from '../../app/js/com/partKart/Global.js';
 import Global  from '../../app/partKart/Global.js';
-
-//var { Global } = require('../app/js/com/partKart/Global');
-
-// SyntaxError: Cannot use import statement outside a module
-//require('../app/js/com/partKart/Global.js');
-//require('../app/js/com/partKart/Global');
-
-// Undefined Global and Global.zoom
-//require('../dist/makercam5.js');
-
-// tests fail
-//import('../app/js/com/partKart/Global.js');
-
-
-// Cannot use import statement outside a module
-//import Global from '../app/js/com/partKart/Global.js';
-
-// Cannot find module 'Global'
-// require('Global');
-
-// Cannot use import statement outside a module
-// require('../app/js/com/partKart/Global');
+import { Point } from '@pixi/math';
 
 describe('Testing Global.js', function ()
 {
@@ -72,16 +11,189 @@ describe('Testing Global.js', function ()
    {
       assert.isNotNull(Global, 'Global resulted in null');
    });
-});
-describe('Testing Global.zoom', function ()
-{
+
+   describe('Testing Global variables', function (){});
+
    it('Global.zoom should be 80', function ()
    {
-            assert.equal(Global.zoom, 80, "Global.zoom = " + Global.zoom);
-      assert.isNotNull(Global, 'Global resulted in null');
+      let expectedResult = 80;
+      assert.equal(Global.zoom, expectedResult, "Global.zoom: expected " + expectedResult + " received = " + Global.zoom);
    });
 
-});
+   it('Global.tool should be: 0', function ()
+   {
+      let expectedResult = 0;
+      assert.equal(Global.tool, expectedResult, "Global.tool: expected " + expectedResult + " received = " + Global.tool);
+   });
 
-// ***************** TEST Plugin Initialized Variables ***************
+   it('Global.snap should be: false', function ()
+   {
+      let expectedResult = false;
+      assert.equal(Global.tool, expectedResult, "Global.snap: expected " + expectedResult + " received = " + Global.snap);
+   });
+
+   it('Global.xorigin should be: 0', function ()
+   {
+      let expectedResult = 0;
+      assert.equal(Global.xorigin, expectedResult, "Global.xorigin: expected " + expectedResult + " received = " + Global.xorigin);
+   });
+
+   it('Global.yorigin should be: 600', function ()
+   {
+      let expectedResult = 600;
+      assert.equal(Global.yorigin, expectedResult, "Global.yorigin: expected " + expectedResult + " received = " + Global.yorigin);
+   });
+
+   it('Global.docwidth should be: 950', function ()
+   {
+      let expectedResult = 950;
+      assert.equal(Global.docwidth, expectedResult, "Global.docwidth: expected " + expectedResult + " received = " + Global.docwidth);
+   });
+
+   it('Global.docheight should be: 600', function ()
+   {
+      let expectedResult = 600;
+      assert.equal(Global.docheight, expectedResult, "Global.docheight: expected " + expectedResult + " received = " + Global.docheight);
+   });
+
+   it('Global.dragging should be: false', function ()
+   {
+      let expectedResult = false;
+      assert.equal(Global.dragging, expectedResult, "Global.dragging: expected " + expectedResult + " received = " + Global.dragging);
+   });
+
+   it('Global.unit should be: in', function ()
+   {
+      let expectedResult = 'in';
+      assert.equal(Global.unit, expectedResult, "Global.unit: expected " + expectedResult + " received = " + Global.unit);
+   });
+
+   it('Global.space should be: false', function ()
+   {
+      let expectedResult = false;
+      assert.equal(Global.space, expectedResult, "Global.space: expected " + expectedResult + " received = " + Global.space);
+   });
+
+   it('Global.importres should be: 72', function ()
+   {
+      let expectedResult = 72;
+      assert.equal(Global.importres, expectedResult, "Global.importres: expected " + expectedResult + " received = " + Global.importres);
+   });
+
+   it('Global.localsnap should be: true', function ()
+   {
+      let expectedResult = true;
+      assert.equal(Global.localsnap, expectedResult, "Global.localsnap: expected " + expectedResult + " received = " + Global.localsnap);
+   });
+
+   it('Global.tolerance should be: .001', function ()
+   {
+      let expectedResult = .001;
+      assert.equal(Global.tolerance, expectedResult, "Global.tolerance: expected " + expectedResult + " received = " + Global.tolerance);
+   });
+
+   it('Global.precision should be: 4', function ()
+   {
+      let expectedResult = 4;
+      assert.equal(Global.precision, expectedResult, "Global.precision: expected " + expectedResult + " received = " + Global.precision);
+   });
+
+   it('Global.bitmaptolerance should be: .007', function ()
+   {
+      let expectedResult = .007;
+      assert.equal(Global.bitmaptolerance, expectedResult, "Global.bitmaptolerance: expected " + expectedResult + " received = " + Global.bitmaptolerance);
+   });
+
+   it('Global.bitmapsize should be: 4000', function ()
+   {
+      let expectedResult = 4000;
+      assert.equal(Global.bitmapsize, expectedResult, "Global.bitmapsize: expected " + expectedResult + " received = " + Global.bitmapsize);
+   });
+
+   it('Global.nestbitmapsize should be: 1000', function ()
+   {
+      let expectedResult = 1000;
+      assert.equal(Global.nestbitmapsize, expectedResult, "Global.nestbitmapsize: expected " + expectedResult + " received = " + Global.nestbitmapsize);
+   });
+
+   it('Global.separatetoolpaths should be: false', function ()
+   {
+      let expectedResult = false;
+      assert.equal(Global.separatetoolpaths, expectedResult, "Global.separatetoolpaths: expected " + expectedResult + " received = " + Global.separatetoolpaths);
+   });
+
+   it('Global.viewcuts should be: false', function ()
+   {
+      let expectedResult = false;
+      assert.equal(Global.viewcuts, expectedResult, "Global.viewcuts: expected " + expectedResult + " received = " + Global.viewcuts);
+   });
+
+   it('Global.undo should be an Array', function ()
+   {
+      assert.isArray(Global.undo, 'Global.undo is not an array');
+   });
+
+   it('Global.undo.length should be: 0', function ()
+   {
+      let expectedResult = 0;
+      assert.equal(Global.undo.length, expectedResult, "Global.undo.length: expected " + expectedResult + " received = " + Global.undo.length);
+   });
+
+   it('Global.undoPush should be a function', function ()
+   {
+      let expectedResult = 'function';
+      assert.isFunction(Global.undoPush, "Global.undoPush: expected " + expectedResult + " received = " + typeof Global.undoPush);
+   });
+
+   it('Global.undoAction should be a function', function ()
+   {
+      let expectedResult = 'function';
+      assert.isFunction(Global.undoAction, "Global.undoAction: expected " + expectedResult + " received = " + typeof Global.undoAction);
+   });
+
+   it('Global.withinTolerance should be a function', function ()
+   {
+      let expectedResult = 'function';
+      assert.isFunction(Global.withinTolerance, "Global.withinTolerance: expected " + expectedResult + " received = " + typeof Global.withinTolerance);
+   });
+
+   it('Global.toFixed should be a function', function ()
+   {
+      let expectedResult = 'function';
+      assert.isFunction(Global.toFixed, "Global.toFixed: expected " + expectedResult + " received = " + typeof Global.toFixed);
+   });
+
+   it('Global.getAngle should be a function', function ()
+   {
+      let expectedResult = 'function';
+      assert.isFunction(Global.getAngle, "Global.getAngle: expected " + expectedResult + " received = " + typeof Global.getAngle);
+   });
+
+   it('new Point(3,4) should create a point at (3,4)', function ()
+   {
+      let P1 = new Point(3,4);
+      assert.equal(P1.x, 3, "Point.x: expected " + 3 + " received = P1.x:" + P1.x + " P1.y:" + P1.y);
+      assert.equal(P1.y, 4, "Point.y: expected " + 4 + " received = " + P1.y);
+   });
+
+   it('Global.getAngle(P0(6,3), P1=(5,13) should return 42° (0.74 rads)', function ()
+   {
+      let P0 = new Point(6,3);
+      let P1 = new Point(5,13);
+      let expectedResult = .74;
+      let result = Global.getAngle(P0,P1);
+      assert.closeTo(result, expectedResult, .05, "Global.getAngle: expected " + expectedResult + " received = " + result);
+   });
+
+
+   it('Global.lineIntersect should be a function', function ()
+   {
+      let expectedResult = 'function';
+      assert.isFunction(Global.lineIntersect, "Global.lineIntersect: expected " + expectedResult + " received = " + typeof Global.lineIntersect);
+   });
+
+
+
+
+});
 
